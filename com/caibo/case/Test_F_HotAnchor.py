d4cbe467f325426024943b04cca5b49c7210dc3b
admin <yan15830921770@163.com>
2019-01-24 18:05:09 +0800

    xiugai

--- com/caibo/case/Test_F_HotAnchor.py
+++ com/caibo/case/Test_F_HotAnchor.py
@@ -4,27 +4,29 @@ from test_caibo_2.com.caibo.api.Log import Log
 from test_caibo_2.com.caibo.business.HotAnchorHelper import HotAnchorHelper
 from test_caibo_2.com.caibo.api.Utils import Utils
 
-#财务记录
+
+# 财务记录
 class Test_HotAnchor(unittest.TestCase):
     
-    #直播间
-    #所有直播间id一样
-    #顶呱呱直播间index:4
-    #合买专场直播间index:10
-    #vip财源滚滚直播间index:9
+    # 直播间
+    # 所有直播间id一样
+    # 顶呱呱直播间index:4
+    # 合买专场直播间index:10
+    # vip财源滚滚直播间index:9
     def ___init___(self):
         print("aaa")
         pass
-    #case_name保持10个字,不够了使用--填充  
-    #点击进入热门主播页
+
+    # case_name保持10个字,不够了使用--填充
+    # 点击进入热门主播页
     def test_hotAnchor1(self):  
         case_name="点击进入热门主播页-"
         Log.print_case_info_My(self,case_name)
         # 调用方法输入手机号,密码，用例名字，截图等待时间，是否初始化driver状态
-        HotAnchorHelper.to_hot_page(self, case_name)#点击我的
+        HotAnchorHelper.to_hot_page(self, case_name)  # 点击我的
         print("本条用例执行通过")
          
-    #点击进入顶呱呱直播间
+    # 点击进入顶呱呱直播间
     def test_hotAnchor2(self):  
         case_name="点击进入顶呱呱直播间"
         Log.print_case_info_My(self,case_name)
